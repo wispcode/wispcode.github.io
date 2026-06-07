fbed630f63b426afe85b44afe4349ad57a15a943
MVCC Web Applications Development <72232931+mvccdev@users.noreply.github.com>
2026-06-06 21:50:56 -0500

    Chore: Updated Documentation

---
+++ assets/pages_components_badge.md.gO4tbt31.js
@@ -0,0 +1,13 @@
+import{_ as e,p as t,d as s,g as o}from"./chunks/framework.D9X38fHW.js";const g=JSON.parse('{"title":"Badge","description":"","frontmatter":{},"headers":[],"relativePath":"pages/components/badge.md","filePath":"pages/components/badge.md"}'),r={name:"pages/components/badge.md"};function n(d,a,l,i,c,p){return t(),s("div",null,[...a[0]||(a[0]=[o(`<h1 id="badge" tabindex="-1">Badge <a class="header-anchor" href="#badge" aria-label="Permalink to &quot;Badge&quot;">​</a></h1><p>Creates a small inline label for status, count, or metadata.</p><pre><code>&lt;span class=&quot;c-badge&quot;&gt;
+	21
+&lt;/span&gt;
+</code></pre><div class="u-vite-preview"><span class="c-badge"> 21 </span></div><h2 id="modifiers" tabindex="-1">Modifiers <a class="header-anchor" href="#modifiers" aria-label="Permalink to &quot;Modifiers&quot;">​</a></h2><h3 id="outline" tabindex="-1">Outline <a class="header-anchor" href="#outline" aria-label="Permalink to &quot;Outline&quot;">​</a></h3><p>Applies a border around the badge and removes the background color.</p><pre><code>&lt;span class=&quot;c-badge c-badge--outline&quot;&gt;
+	21
+&lt;/span&gt;
+</code></pre><div class="u-vite-preview"><span class="c-badge c-badge--outline"> 21 </span></div><h3 id="square" tabindex="-1">Square <a class="header-anchor" href="#square" aria-label="Permalink to &quot;Square&quot;">​</a></h3><p>Applies a smaller border radius, making it more square-shaped.</p><pre><code>&lt;span class=&quot;c-badge c-badge--square&quot;&gt;
+	21
+&lt;/span&gt;
+</code></pre><div class="u-vite-preview"><span class="c-badge c-badge--square"> 21 </span></div><h2 id="notes" tabindex="-1">Notes <a class="header-anchor" href="#notes" aria-label="Permalink to &quot;Notes&quot;">​</a></h2><h3 id="colors" tabindex="-1">Colors <a class="header-anchor" href="#colors" aria-label="Permalink to &quot;Colors&quot;">​</a></h3><p>Use color utilities or custom color classes to change the color.</p><pre><code>&lt;span class=&quot;c-badge u-color-primary&quot;&gt;
+	21
+&lt;/span&gt;
+</code></pre><div class="u-vite-preview"><span class="c-badge u-color-primary"> 21 </span></div>`,18)])])}const h=e(r,[["render",n]]);export{g as __pageData,h as default};
