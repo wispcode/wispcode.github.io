aac0c3261eaf01a1de62860c4a9915533de0219b
MVCC Web Applications Development <wispcss@proton.me>
2026-06-09 12:24:13 -0500

    Chore: Updated Documentation

---
+++ assets/pages_components_button-group.md.CTFheSRL.js
@@ -0,0 +1,6 @@
+import{_ as o,p as n,d as e,g as u}from"./chunks/framework.D9X38fHW.js";const i=JSON.parse('{"title":"Button Group","description":"","frontmatter":{},"headers":[],"relativePath":"pages/components/button-group.md","filePath":"pages/components/button-group.md"}'),s={name:"pages/components/button-group.md"};function a(r,t,c,p,b,l){return n(),e("div",null,[...t[0]||(t[0]=[u(`<h1 id="button-group" tabindex="-1">Button Group <a class="header-anchor" href="#button-group" aria-label="Permalink to &quot;Button Group&quot;">​</a></h1><p>Removes space and border-radius where buttons meet.</p><pre><code>&lt;div class=&quot;c-button-group&quot;&gt;
+	&lt;button class=&quot;c-button&quot;&gt;Item 1&lt;/button&gt;		
+	&lt;button class=&quot;c-button&quot;&gt;Item 2&lt;/button&gt;
+	&lt;button class=&quot;c-button&quot;&gt;Item 3&lt;/button&gt;
+&lt;/div&gt;
+</code></pre><div class="u-vite-preview"><div class="c-button-group"><button class="c-button">Item 1</button><button class="c-button">Item 2</button><button class="c-button">Item 3</button></div></div>`,4)])])}const m=o(s,[["render",a]]);export{i as __pageData,m as default};
